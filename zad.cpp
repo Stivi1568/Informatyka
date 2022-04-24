99d49252eebf3a6c6a02b428ea10ee95db5412c9
Stivi1568 <75080495+Stivi1568@users.noreply.github.com>
2022-04-24 23:25:06 +0200

    Add files via upload

---
+++ zad.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#include <string>
+
+
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+    int przesuniecie;                                                            
+
+    string szyfrogram;                                                          
+
+    cout << "Podaj przesuniecie: ";
+
+    cin >> przesuniecie;                                                       
+
+    cout << "Podaj szyfr : ";
+
+    cin >> szyfrogram;                                           
+
+
+
+    cout << "Zakodowana wiadomosc to: " << endl;
+
+    for (char litera : szyfrogram)                                             
+
+    {
+
+        char zakodowana_litera = 65 + (litera - 65 + przesuniecie) % 26;
+
+        cout << zakodowana_litera;                                              
+
+    }
+
+
+
+    return 0;
+
+}
